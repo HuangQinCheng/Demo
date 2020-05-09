217b0c2f6886b89c3d61634d315af6df5566e48a
Huang Qin Cheng <qinchenghuang@outlook.com>
2020-05-09 15:35:56 +0800

    命令行项目添加委托方法测试

---
+++ ConsoleApp1/ListExt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class ListExt
+    {
+        public static List<T> MyFindAll<T>(this List<T> lis, Func<T, bool> func)
+        {
+            List<T> result = new List<T>();
+            foreach (var item in lis)
+            {
+                if (func(item))
+                {
+                    result.Add(item);
+                }
+
+            }
+            return result;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            List<string> list = new List<string>()
+            {
+                "2","1","5","4","3"
+            };
+            //var result=list.FindAll(str => str.CompareTo("2") > 0);
+            var result = list.MyFindAll(str => str.CompareTo("2") > 0);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
         }
     }
 }
